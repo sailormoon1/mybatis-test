d021bdc7840c5e78109d03729efbb610839376da
Tiffany <guomengmeng@allinmd.cn>
2021-03-02 10:02:25 +0800

    pom.xml 修改点：指定resource文件路径，否则target文件夹下的java文件夹中的xml文件不生效
    2.添加oracle驱动类jar包

--- src/test/java/com/example/mybatistest/demo/MybatisTest.java
+++ src/test/java/com/example/mybatistest/demo/MybatisTest.java
@@ -13,12 +13,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author guomengmeng
  * @Version 1.0.0
  * @date 2021/2/25 2:16 下午
  * @description 类描述
  */
+@Slf4j
 public class MybatisTest {
     @Test
     public void testMyBatisBuild() throws IOException {
@@ -27,5 +30,6 @@ public class MybatisTest {
         SqlSession sqlSession = sessionFactory.openSession();
         IClcDiagMapper mapper = sqlSession.getMapper(IClcDiagMapper.class);
         ClcDiagEntity student = mapper.queryByDiagId("89ffcbd7a3a44db8a0b40a2bbffff452");
+        log.info("查询到的信息为：{}",student);
     }
 }
